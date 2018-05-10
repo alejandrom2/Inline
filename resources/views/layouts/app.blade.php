fe4f9b0250684616531c0206d65643cc1e6a4f01
Alejandro Martinez <hello@alejandrom.xyz>
2018-05-10 11:16:56 -0700

    Changes to nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,228 +64,61 @@
           <li class="active">
             <a href="../examples/dashboard.html">
               <i class="now-ui-icons design_app"></i>
-              <p>Dashboard</p>
+              <p>Tasks</p>
             </a>
           </li>
           <li>
-            <a data-toggle="collapse" href="#pagesExamples">
-              <i class="now-ui-icons design_image"></i>
-              <p>
-                Pages
-                <b class="caret"></b>
-              </p>
-            </a>
-            <div class="collapse " id="pagesExamples">
-              <ul class="nav">
-                <li>
-                  <a href="../examples/pages/pricing.html">
-                    <span class="sidebar-mini-icon">P</span>
-                    <span class="sidebar-normal"> Pricing </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="../examples/pages/timeline.html">
-                    <span class="sidebar-mini-icon">T</span>
-                    <span class="sidebar-normal"> Timeline </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="../examples/pages/login.html">
-                    <span class="sidebar-mini-icon">L</span>
-                    <span class="sidebar-normal"> Login </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="../examples/pages/register.html">
-                    <span class="sidebar-mini-icon">R</span>
-                    <span class="sidebar-normal"> Register </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="../examples/pages/lock.html">
-                    <span class="sidebar-mini-icon">LS</span>
-                    <span class="sidebar-normal"> Lock Screen </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="../examples/pages/user.html">
-                    <span class="sidebar-mini-icon">UP</span>
-                    <span class="sidebar-normal"> User Profile </span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <a data-toggle="collapse" href="#componentsExamples">
-              <i class="now-ui-icons education_atom"></i>
-              <p>
-                Components
-                <b class="caret"></b>
-              </p>
-            </a>
-            <div class="collapse " id="componentsExamples">
-              <ul class="nav">
-                <li>
-                  <a href="../examples/components/buttons.html">
-                    <span class="sidebar-mini-icon">B</span>
-                    <span class="sidebar-normal"> Buttons </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="../examples/components/grid.html">
-                    <span class="sidebar-mini-icon">G</span>
-                    <span class="sidebar-normal"> Grid System </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="../examples/components/panels.html">
-                    <span class="sidebar-mini-icon">P</span>
-                    <span class="sidebar-normal"> Panels </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="../examples/components/sweet-alert.html">
-                    <span class="sidebar-mini-icon">SA</span>
-                    <span class="sidebar-normal"> Sweet Alert </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="../examples/components/notifications.html">
-                    <span class="sidebar-mini-icon">N</span>
-                    <span class="sidebar-normal"> Notifications </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="../examples/components/icons.html">
-                    <span class="sidebar-mini-icon">I</span>
-                    <span class="sidebar-normal"> Icons </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="../examples/components/typography.html">
-                    <span class="sidebar-mini-icon">T</span>
-                    <span class="sidebar-normal"> Typography </span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <a data-toggle="collapse" href="#formsExamples">
-              <i class="now-ui-icons files_single-copy-04"></i>
-              <p>
-                Forms
-                <b class="caret"></b>
-              </p>
-            </a>
-            <div class="collapse " id="formsExamples">
-              <ul class="nav">
-                <li>
-                  <a href="../examples/forms/regular.html">
-                    <span class="sidebar-mini-icon">RF</span>
-                    <span class="sidebar-normal"> Regular Forms </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="../examples/forms/extended.html">
-                    <span class="sidebar-mini-icon">EF</span>
-                    <span class="sidebar-normal"> Extended Forms </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="../examples/forms/validation.html">
-                    <span class="sidebar-mini-icon">V</span>
-                    <span class="sidebar-normal"> Validation Forms </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="../examples/forms/wizard.html">
-                    <span class="sidebar-mini-icon">W</span>
-                    <span class="sidebar-normal"> Wizard </span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <a data-toggle="collapse" href="#tablesExamples">
-              <i class="now-ui-icons design_bullet-list-67"></i>
-              <p>
-                Tables
-                <b class="caret"></b>
-              </p>
-            </a>
-            <div class="collapse " id="tablesExamples">
-              <ul class="nav">
-                <li>
-                  <a href="../examples/tables/regular.html">
-                    <span class="sidebar-mini-icon">RT</span>
-                    <span class="sidebar-normal"> Regular Tables </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="../examples/tables/extended.html">
-                    <span class="sidebar-mini-icon">ET</span>
-                    <span class="sidebar-normal"> Extended Tables </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="../examples/tables/datatables.net.html">
-                    <span class="sidebar-mini-icon">DT</span>
-                    <span class="sidebar-normal"> DataTables.net </span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <a data-toggle="collapse" href="#mapsExamples">
-              <i class="now-ui-icons location_pin"></i>
-              <p>
-                Maps
-                <b class="caret"></b>
-              </p>
-            </a>
-            <div class="collapse " id="mapsExamples">
-              <ul class="nav">
-                <li>
-                  <a href="../examples/maps/google.html">
-                    <span class="sidebar-mini-icon">GM</span>
-                    <span class="sidebar-normal"> Google Maps </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="../examples/maps/fullscreen.html">
-                    <span class="sidebar-mini-icon">FM</span>
-                    <span class="sidebar-normal"> Full Screen Map </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="../examples/maps/vector.html">
-                    <span class="sidebar-mini-icon">VM</span>
-                    <span class="sidebar-normal"> Vector Map </span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </li>
-          <li>
-            <a href="../examples/widgets.html">
-              <i class="now-ui-icons objects_diamond"></i>
-              <p>Widgets</p>
+            <a href="../examples/dashboard.html">
+              <i class="now-ui-icons design_app"></i>
+              <p>Deliverables</p>
             </a>
           </li>
           <li>
-            <a href="../examples/charts.html">
-              <i class="now-ui-icons business_chart-pie-36"></i>
-              <p>Charts</p>
+            <a href="../examples/dashboard.html">
+              <i class="now-ui-icons design_app"></i>
+              <p>Action Items</p>
             </a>
           </li>
           <li>
-            <a href="../examples/calendar.html">
-              <i class="now-ui-icons media-1_album"></i>
-              <p>Calendar</p>
+            <a href="../examples/dashboard.html">
+              <i class="now-ui-icons design_app"></i>
+              <p>Changes</p>
+            </a>
+          </li>
+          <li>
+            <a href="../examples/dashboard.html">
+              <i class="now-ui-icons design_app"></i>
+              <p>Issues</p>
+            </a>
+          </li>
+          <li>
+            <a href="../examples/dashboard.html">
+              <i class="now-ui-icons design_app"></i>
+              <p>Resources</p>
+            </a>
+          </li>
+          <li>
+            <a href="../examples/dashboard.html">
+              <i class="now-ui-icons design_app"></i>
+              <p>Decisions</p>
+            </a>
+          </li>
+          <li>
+            <a href="../examples/dashboard.html">
+              <i class="now-ui-icons design_app"></i>
+              <p>Risks</p>
+            </a>
+          </li>
+          <li>
+            <a href="../examples/dashboard.html">
+              <i class="now-ui-icons design_app"></i>
+              <p>Reference Documents</p>
+            </a>
+          </li>
+          <li>
+            <a href="../examples/dashboard.html">
+              <i class="now-ui-icons design_app"></i>
+              <p>Requirements</p>
             </a>
           </li>
         </ul>
@@ -320,34 +153,26 @@
               </div>
             </form>
             <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="#pablo">
-                  <i class="now-ui-icons media-2_sound-wave"></i>
-                  <p>
-                    <span class="d-lg-none d-md-block">Stats</span>
-                  </p>
-                </a>
-              </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="now-ui-icons location_world"></i>
-                  <p>
-                    <span class="d-lg-none d-md-block">Some Actions</span>
-                  </p>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                  <a class="dropdown-item" href="#">Action</a>
-                  <a class="dropdown-item" href="#">Another action</a>
-                  <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#pablo">
                   <i class="now-ui-icons users_single-02"></i>
                   <p>
                     <span class="d-lg-none d-md-block">Account</span>
                   </p>
                 </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                  @auth
+                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                  @else
+                  <a class="dropdown-item" href="{{ route('login') }}">Login</a>
+                  <a class="dropdown-item" href="{{ route('register') }}">Register</a>
+                  @endauth
+                </div>
               </li>
             </ul>
           </div>
